2b1d071d844ebc5711c7b310eff2d2c405214298
hub33k <hub33k@yahoo.com>
2021-07-04 12:52:24 +0200

    Add cross-env and prettier.

---
+++ .prettierrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  useTabs: false,
+  printWidth: 100,
+  tabWidth: 2,
+
+  semi: true,
+  trailingComma: 'all',
+  singleQuote: true,
+  arrowParens: 'always',
+  bracketSpacing: true,
+
+  jsxBracketSameLine: false,
+  jsxSingleQuote: false,
+
+  proseWrap: 'never',
+};
